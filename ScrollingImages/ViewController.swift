18e753c483093b75f0c011b419ca774d8dad9312
Elizaveta <90791523+gallelizabeth@users.noreply.github.com>
2026-07-31 14:56:44 +0400

    Initial Commit

---
+++ ScrollingImages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollingImages
+//
+//  Created by elizabeth gall on 31.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScrollingImagesTests/ScrollingImagesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingImagesTests.swift
+//  ScrollingImagesTests
+//
+//  Created by elizabeth gall on 31.07.2026.
+//
+
+import Testing
+@testable import ScrollingImages
+
+struct ScrollingImagesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
